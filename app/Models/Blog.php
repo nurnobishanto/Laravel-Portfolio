5b7058b0b105161e40c208f85922eae2273c030f
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2023-01-26 22:00:21 +0600

    update Blog 1.0

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Blog extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'id',
+        'category_id',
+        'name',
+        'slug',
+        'featured_image',
+        'body',
+    ];
 }
