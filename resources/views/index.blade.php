a2930f6a7a271077e791047246b0bf5aaad54d23
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2023-01-27 11:19:42 +0600

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1108,9 +1108,9 @@
                     <div class="dizme_tm_map wow fadeInUp" data-wow-duration="1s">
                         <div class="mapouter">
                             <div class="gmap_canvas"><iframe width="100%" height="375" id="gmap_canvas"
-                                    src="https://maps.google.com/maps?q=2880%20Broadway,%20New%20York&t=&z=13&ie=UTF8&iwloc=&output=embed"
+                                    src="{{setting('my.map')}}"
                                     frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a
-                                    href="{{setting('my.map')}}"></a><br>
+                                    href="https://www.embedgooglemap.net/blog/divi-discount-code-elegant-themes-coupon"></a><br>
                                 <style>
                                     .mapouter {
                                         position: relative;
